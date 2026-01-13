99b5da19082d9f3a2d4227c3fbb013e9972898ad
Syeda Alishba <syedaw.Alishba@gmail.com>
2026-01-13 18:04:25 +0500

    Add files via upload

---
+++ LinearSearch/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+void LinearSearch(int array[],int size, int target) {
+    int index = -1;
+    for (int i = 0; i < size; i++) {
+        if (array[i]==target) {
+            cout << "FOUNT at index "<<i ;
+            index = i;
+        }
+    }
+    if (index == -1) {
+        cout << "NOT FOUND" << endl;
+    }
+}
+int main() {
+
+   int array[] = {1,2,3,4,5,6,7,8,9};
+    LinearSearch(array,9,6);
+}
