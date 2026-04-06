3bb921b13ca82890c271d1bc6b5ba94b5f505e9c
Syeda Alishba <syedaw.Alishba@gmail.com>
2026-04-06 23:41:27 +0500

    Update SearchEngine.cpp

--- MiniSearchEngine/SearchEngine.cpp
+++ MiniSearchEngine/SearchEngine.cpp
@@ -1,30 +1,28 @@
-#include <iostream>
-#include <string>
-#include <vector>
+#include "SearchEngine.h"
 #include "DocumentLoader.h"
+#include <iostream>
+
 using namespace std;
-class SearchEngine {
-    public:
-    vector<string> documents;
-
-    void loadDocuments();
-
-    void displayDocuments();
-};
 
 void SearchEngine::loadDocuments() {
     DocumentLoader loader;
 
-    // List of files (you can later make this dynamic)
+    // You can later make this dynamic
     vector<string> files = {"doc1.txt", "doc2.txt", "doc3.txt"};
 
     for (string file : files) {
         string content = loader.readFile(file);
 
-        // Store only if file is not empty
         if (content != "") {
             documents.push_back(content);
         }
+    }
+}
+
+void SearchEngine::displayDocuments() {
+    for (int i = 0; i < documents.size(); i++) {
+        cout << "Document " << i + 1 << ":\n";
+        cout << documents[i] << "\n\n";
     }
 }
 void SearchEngine::displayDocuments() {
